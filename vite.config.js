8c0cc7a14171ccb5497bea2cec26f92e239c44b9
thekhuong97 <thekhuong97@gmail.com>
2025-05-14 19:00:19 +0700

    First Commit

---
+++ vite.config.js
@@ -0,0 +1,23 @@
+import { defineConfig } from "vite";
+import { ViteEjsPlugin } from "vite-plugin-ejs";
+import path from "path";
+
+export default defineConfig({
+  root: "src",
+  publicDir: "../public",
+  build: {
+    outDir: "../dist",
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        index: path.resolve(__dirname, "src/index.html"),
+        signin: path.resolve(__dirname, "src/sign-in.html"),
+        signup: path.resolve(__dirname, "src/sign-up.html"),
+        account: path.resolve(__dirname, "src/account.html"),
+        collection: path.resolve(__dirname, "src/collection.html"),
+        payment: path.resolve(__dirname, "src/payment.html"),
+      },
+    },
+  },
+  plugins: [ViteEjsPlugin()],
+});
